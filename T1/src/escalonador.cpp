6d0fa8763a8efe50d8af7079b1d1fe6f0408523b
lucassmoro <lucas.sm.souza@gmail.com>
2025-04-22 08:38:42 -0300

    I/O bound

--- T1/src/escalonador.cpp
+++ T1/src/escalonador.cpp
@@ -15,11 +15,11 @@
         while (!processos.empty()){
             processo_execucao = processos.front();
             processos.pop_front();
-            execucao_processo(processos);
             std::cout<<"Fila de execucao: Processo em execucao: "<<processo_execucao.id<<std::endl;
+            execucao_processo(processos);
         }
     }
-    void Escalonador::escalonar_prioridade(std::deque<Processo>& processos)
+    void Escalonador::escalonar_prioridade(std::deque<Processo>& processos) //escalonamento por prioridade
     {
         std::sort(processos.begin(), processos.end(),
             [](const Processo& p1, Processo& p2){
@@ -27,16 +27,30 @@
             }
         );
     }
+
     void Escalonador::execucao_processo(std::deque<Processo>& processos)
     {
+        int ciclos_executados = 0;
         while (processo_execucao.tempo_estimado > 0) {
             std::cout<<"processo execucao tempo estimado "<<processo_execucao.tempo_estimado<<std::endl;
             sleep(1);
             processo_execucao.tempo_estimado--;
+            ciclos_executados++;
+
+            // I/O-Bound
+            if (processo_execucao.perfil == "IO-Bound" && ciclos_executados == 2) {
+                std::cout << "Processo " << processo_execucao.id << " bloqueado por I/O\n";
+                ciclos_executados = 0;
+                processos.push_back(processo_execucao);
+                return;
+            }
+
             for (size_t i = 0; i < processos.size(); i++){
                 if (processos[i].prioridade > processo_execucao.prioridade){
+                    processos.push_back(processo_execucao);
                     escalonar_prioridade(processos);
                     processo_execucao = processos[0];
+                    return;
                 };
             }
         }
